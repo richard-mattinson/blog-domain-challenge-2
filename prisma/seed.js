06c6f753866f0f7649c9b3ad6a173423cc440445
Richard Mattinson <richard.m@ttinson.net>
2022-09-01 16:37:47 +0100

    user seed created

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,22 +3,23 @@ const prisma = new PrismaClient();
 
 async function seed() {
     await createUser();
-    const profiles = await createUser();
-    const posts = await createProfile();
-    const categories = await createPosts(); 
-    await createComments(posts, profiles);
+    // const profiles = await createUser();
+    // const posts = await createProfile();
+    // const categories = await createPosts(); 
+    // await createComments(posts, profiles);
     process.exit(0);
 }
 
 async function createUser() {
     const user = await prisma.user.create({
+        data: {
         username: 'Des',
         email: 'thewonkymess@gds.co.uk',
         password: 'youCheekyBOOM365',
-    })
+        }
+    });
 
     console.log("User created", user);
-
     return user;
 }
 
